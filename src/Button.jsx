880eaedf182f032680f86384f64bad192f17bb93
Anuj Kumar <personeluse2000@gmail.com>
2026-04-20 13:22:56 +0530

    button created

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
-const Button = () => {
-  return (
-    <div>Button</div>
-  )
+function Button({
+  children,
+  type = 'button',
+  textColor = 'text-white',
+  bgColor = 'bg-blue-600',
+  className = '',
+  ...props
+} ) {
+    return(
+      <button className={`px-4 py-2 rounded-lg ${textColor} ${className} ${bgColor}
+      ${props}`}>
+        {children}
+      </button>
+    )
 }
 
 export default Button
